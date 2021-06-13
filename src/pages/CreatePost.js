5103d0d58bf95847a4a248374415761ec6a73738
kajimura <k2hmr94@gmail.com>
2021-06-13 13:16:47 +0900

    投稿作成機能が途中

--- src/pages/CreatePost.js
+++ src/pages/CreatePost.js
@@ -1,9 +1,54 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import Axios from 'axios';
+import "../App.css";
 
 function CreatePost() {
+  const [userName, setUserName] = useState("");
+  const [title, setTitle] = useState("");
+  const [text, setText] = useState("");
+
+  const initialValues = {
+    title: "",
+    postText: "",
+    username: "",
+  };
+
+  // useEffect(() => {
+  //   console.log(userName + title + text)
+  // }, [userName, title, text])
+
+  const submitPost = (data) => {
+    Axios.post("http://localhost:3001/api/create", data).then((response) => {
+      console.log("IT WORKED");
+    });
+  };
+
   return (
     <div className="CreatePost">
-      <h1>CreatePost</h1>
+      <div className="uploadPost">
+        <label>UserName</label>
+        <input
+          type="text"
+          onChange={(e) => {
+            setUserName(e.target.value);
+          }}
+        />
+        <label>Title</label>
+        <input
+          type="text"
+          onChange={(e) => {
+            setTitle(e.target.value);
+          }}
+        />
+        <label>Post Text</label>
+        <textarea
+          onChange={(e) => {
+            setText(e.target.value);
+          }}
+        />
+
+        <button onClick={submitPost}> Submit Post</button>
+      </div>
     </div>
   )
 }
